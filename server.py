3604bd9fdde96499aa8ee573d5433c297819b90b
Nick Rushton <nrushton@qualtrics.com>
2018-10-31 13:08:40 -0700

    make serial connection more robust to interrupts (e.g. unplug laptop, laptop goes to sleep, etc)

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 from serial.tools.list_ports import comports
+from serial import SerialException
 import serial
 import struct
 import time
 from flask import Flask
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
 api = Flask(__name__)
 
-usb = serial.serial_for_url([p.device for p in comports() if 'usbserial' in p.device][0])
-usb.timeout = .2
+usb = None
 
 @api.route("/<int:address>/<int:red>/<int:green>/<int:blue>")
 def set_led(address, red, green, blue):
@@ -19,17 +24,27 @@ def transition_led(address, red, green, blue, duration):
     return "done!"
 
 def set_led(address, red, green, blue, duration):
+    global usb
     ackd = False
+    #message format: <led_address:2-byte-int><red:2-byte-int><green:2-byte-int><blue:2-byte-int><intensity:2-byte-int>
+    msg = struct.pack('5H', address, red, green ,blue, duration)
     # try writing until the microcontroller sends an ACK
     # message back on the serial line
     while not ackd:
+        try:
+            usb.write(msg)
+            ack_msg = usb.readline()
+            if b'ACK' in ack_msg:
+                ackd = True
+        except SerialException:
+            logger.info("Caught serial exception, reinitializing serial conneciton.")
+            usb = get_serial_connection()
 
-        #message format: <led_address:2-byte-int><red:2-byte-int><green:2-byte-int><blue:2-byte-int><intensity:2-byte-int>
-        msg = struct.pack('5H', address, red, green ,blue, duration)
-        usb.write(msg)
-        ack_msg = usb.readline()
-        if b'ACK' in ack_msg:
-            ackd = True
+def get_serial_connection():
+    usb = serial.serial_for_url([p.device for p in comports() if 'usbserial' in p.device][0])
+    usb.timeout = .2
+    return usb
 
 if __name__ == '__main__':
+    usb = get_serial_connection()
     api.run()
